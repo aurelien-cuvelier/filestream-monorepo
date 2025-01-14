9515feb3bc4c2f264e382d5d637c9b95e083cc02
ac57100 <aurelien.cuvelier@gmail.com>
2025-01-14 19:39:32 +0300

    layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,58 @@
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Layout, message, Upload, UploadFile, UploadProps } from "antd";
+import { useState } from "react";
 import "./App.css";
 
+const { Content } = Layout;
+
 function App() {
-  return <></>;
+  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const [uploading, setUploading] = useState(false);
+
+  const handleUpload = () => {
+    setUploading(true);
+    // You can use any AJAX library you like
+
+    message.success("upload successfully.");
+    //message.error("upload failed.");
+    setUploading(false);
+  };
+
+  const props: UploadProps = {
+    onRemove: (file) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    beforeUpload: (file) => {
+      setFileList([...fileList, file]);
+
+      return false;
+    },
+    fileList,
+  };
+
+  return (
+    <>
+      <div className="flex justify-center items-center w-screen h-screen">
+        <Content className="flex flex-col justify-center items-center text-white text-3xl">
+          <Upload {...props}>
+            <Button icon={<UploadOutlined />}>Select File</Button>
+          </Upload>
+          <Button
+            type="primary"
+            onClick={handleUpload}
+            disabled={fileList.length === 0}
+            loading={uploading}
+            style={{ marginTop: 16, color: "white" }}
+          >
+            {uploading ? "Uploading" : "Start Upload"}
+          </Button>
+        </Content>
+      </div>
+    </>
+  );
 }
 
 export default App;
